3515d7fb37ea333469fc7b29c3a7c2dd849eea42
Shivam Sharma <shivi08567@gmail.com>
2021-05-05 11:56:41 +0530

    fix CORS Error

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,67 +1,75 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import PlantList from './PlantList/PlantList';
-import Pagination from './Pagination/Pagination';
-import './Api.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PlantList from "./PlantList/PlantList";
+import Pagination from "./Pagination/Pagination";
+import "./Api.css";
 
+const Api = () => {
+  const ACCESS_TOKEN = "AJUcraEaYxH-sVl2C6wbHGi0GVj6P9Cuf0DDCb3IDp8";
 
+  const [plant, setPlant] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPageURL, setCurrentPageURL] = useState(
+    `https://frozen-anchorage-52262.herokuapp.com/https://trefle.io/api/v1/plants?token=${ACCESS_TOKEN}`
+  );
+  const [nextPageURL, setNextPageURL] = useState();
+  const [prevPageURL, setPrevPageURL] = useState();
 
-const Api = () =>  {
-    
-    const ACCESS_TOKEN = "AJUcraEaYxH-sVl2C6wbHGi0GVj6P9Cuf0DDCb3IDp8"
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
 
-    const [plant, setPlant] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [currentPageURL, setCurrentPageURL] = useState(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=${ACCESS_TOKEN}`)
-    const [nextPageURL, setNextPageURL] = useState()
-    const [prevPageURL, setPrevPageURL] = useState()
+    axios
+      .get(currentPageURL, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((response) => {
+        setLoading(false);
+        setPlant(response.data.data);
+        setNextPageURL(
+          `https://frozen-anchorage-52262.herokuapp.com/https://trefle.io${response.data.links.next}&token=${ACCESS_TOKEN}`
+        );
 
-    useEffect(() => {
-        setLoading(true)
-        let cancel
+        if (response.data.links.prev != undefined)
+          setPrevPageURL(
+            `https://frozen-anchorage-52262.herokuapp.com/https://trefle.io${response.data.links.prev}&token=${ACCESS_TOKEN}`
+          );
+        else setPrevPageURL(null);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+      });
 
-        axios.get(currentPageURL, {
-            cancelToken: new axios.CancelToken(c => cancel = c)
-        })
-        .then(response => {
-            setLoading(false)
-            setPlant(response.data.data)
-            setNextPageURL(`https://cors-anywhere.herokuapp.com/https://trefle.io${response.data.links.next}&token=${ACCESS_TOKEN}`)
+    return () => cancel();
+  }, [currentPageURL]);
 
-            if(response.data.links.prev != undefined)
-                setPrevPageURL(`https://cors-anywhere.herokuapp.com/https://trefle.io${response.data.links.prev}&token=${ACCESS_TOKEN}`)
-            else
-                setPrevPageURL(null)
-        })
-        .catch(error => {
-            setLoading(false)
-            console.log(error)
-        })
-
-        return () => cancel()
-    },[currentPageURL])  
-
-
-    if(loading){
-        return <img className = "loading-gif" src = "https://cdn.dribbble.com/users/241278/screenshots/2438365/color-fill.gif" />
-    }
-
-    function gotoNextPage(){
-        setCurrentPageURL(nextPageURL)
-    }
-
-    function gotoPrevPage() {
-        setCurrentPageURL(prevPageURL)
-    }
-
+  if (loading) {
     return (
-        <> 
-             <PlantList plant = {plant} />
-             <Pagination
-              gotoNextPage = {nextPageURL ? gotoNextPage : null} 
-              gotoPrevPage = {prevPageURL ? gotoPrevPage : null}/>
-        </>
-    )
-}
+      <img
+        className="loading-gif"
+        src="https://cdn.dribbble.com/users/241278/screenshots/2438365/color-fill.gif"
+      />
+    );
+  }
 
-export default Api
+  function gotoNextPage() {
+    setCurrentPageURL(nextPageURL);
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageURL(prevPageURL);
+  }
+
+  return (
+    <>
+      <PlantList plant={plant} />
+      <Pagination
+        gotoNextPage={nextPageURL ? gotoNextPage : null}
+        gotoPrevPage={prevPageURL ? gotoPrevPage : null}
+      />
+    </>
+  );
+};
+
+export default Api;
